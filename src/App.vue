977d350c88659ccd2cb906275e37763a627ba6fd
madortill <mador.till@gmail.com>
2022-12-28 15:29:51 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div id="app">
+        <HelloWorld msg="You taught me secert language I can't speak with anyone else ):" />
+  </div>
+</template>
+
+<style scoped>
+  
+</style>
